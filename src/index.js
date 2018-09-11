825be94a0426e718c78e69dc2f90c1b26b00f92e
Martin <martbelmoaw@gmail.com>
2018-09-11 20:40:38 +0300

    working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-    let result = 0;
+    if(base <= number){
+        let counter = 0;
 
-    for (let i = 5; parseInt(number / i) > 0; i *= 5) {
-        result += parseInt(number / i);
+        for(let i = 5; parseInt(number / i) > 0; i *= 5){
+            counter += parseInt(number / i);
+        }
+
+        return counter;
+    } else {
+
     }
 
-    return result;
+    let based_num = 0;
+    let str = '';
+
+    while(number > 0){
+        str += number % base;
+        number = parseInt(number / base);
+    }
+
+    based_num = parseInt(str.split('').reverse().join(''));
 }
